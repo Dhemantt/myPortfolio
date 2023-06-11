18b604b9581b8fd1e0de017bc5b7d87cad03f648
hemant.kumar.dixit <hemant.kumar.dixit@accenture.com>
2023-06-11 17:47:15 +0530

    About section complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import SocialLink from "./components/SocialLink";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home/>
+      <About/>
+      <Portfolio/>
 
       <SocialLink/>
     </div>
